a449e43a0c6b8d038e9f39de48a5ddacc5e6a1c2
Sinitsyn Daniil <80570939+Sib-Coder@users.noreply.github.com>
2023-04-04 12:43:15 +0700

    add algoritm

--- main.go
+++ main.go
@@ -13,23 +13,48 @@ func main() {
 	myurl := "http://localhost:9980/"
 	//data := RequestToUrl(myurl)
 	//fmt.Println(data)
-	patern := "777777"
-	chek1 := CheckPusReq(patern, myurl)
-	fmt.Println(chek1)
+	//patern := "777777"
+	//chek1 := CheckPusReq(patern, myurl)
+	//fmt.Println(chek1)
+	Algoritm(myurl)
 
 }
 
-// func RequestToUrl(myurl string) string { //можно объединить 2 эти функции в 1 и сделать единую функцию с входными параметрами url и патерн
-//
-//		resp, _ := http.Get(myurl + "{{7*'7'}}")
-//		bytes, _ := ioutil.ReadAll(resp.Body)
-//		Body := string(bytes)
-//		return Body
-//	}
-func CheckPusReq(patern string, myurl string) bool {
-	resp, _ := http.Get(myurl + "{{7*'7'}}")
+func CheckPusReq(patern string, myurl string, path string) bool {
+	resp, _ := http.Get(myurl + path) // переделать 2 строку на тестовый путь
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	Body := string(bytes)
 	matched, _ := regexp.MatchString(patern, Body) // пустой «приемник» ошибки, ведь мы уверены, что пример отработает нормально
+	fmt.Println(patern)
 	return matched
+}
+func Algoritm(url string) {
+	path1 := "${7*7}" //это не патерны - это тестовые пути
+	path2 := "a{*comment*}b"
+	path3 := "${\"z\".join(\"ab\")}" //пофиксить строку
+	path4 := "{{7*7}}"
+	path5 := "{{7*'7'}}"
+
+	/// заменить верхнее на патерны
+	patern1 := "${7*7}" //это не патерны - это тестовые пути
+	patern2 := "a{*comment*}b"
+	patern3 := "${\"z\".join(\"ab\")}" //пофиксить строку
+	patern4 := "49"
+	patern5 := "7777777"
+
+	if CheckPusReq(patern1, url, path1) == true {
+		if CheckPusReq(patern2, url, path2) == true {
+			fmt.Println("Smarty")
+		} else if CheckPusReq(patern3, url, path3) == true {
+			fmt.Println("Mako")
+		}
+	} else if CheckPusReq(patern4, url, path4) == true {
+		if CheckPusReq(patern5, url, path5) == true {
+			fmt.Println("Jinja2 and Twig")
+		} else {
+			fmt.Println("Unknown")
+		}
+	} else {
+		fmt.Println("Не знаю")
+	}
 }
